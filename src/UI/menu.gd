e85f6aa40f00141ce6fe5480029230e371c23c5e
KWTYZ\16496 <1649608613@qq.com>
2025-01-01 02:01:23 +0800

    新增666欢迎语

--- src/UI/menu.gd
+++ src/UI/menu.gd
@@ -3,6 +3,7 @@ var mainScene=preload("res://src/game/main.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("666666666")
 	pass # Replace with function body.
 
 func _on_start_pressed() -> void:
